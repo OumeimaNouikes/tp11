ad54d56bd9066f3d3fe83a3b2b3f4924f38eb9ed
POST5 <post5.prog2@enetcom.usf.tn>
2023-09-18 16:25:06 +0100

    first commit

---
+++ app/src/main/java/com/example/myapplicatio/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplicatio;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplicatio.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplicatio/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.example.myapplicatio.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
